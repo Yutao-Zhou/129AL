7171861d55c951582277c3b043172f5d2e275716
Yutao-Zhou <13520759678@163.com>
2021-09-23 22:42:11 -0700

    initial

---
+++ 1User_Input.py
@@ -0,0 +1,3 @@
+string = input ("Enter string: ") # Asking user to give string input
+for line in range(10): # making a loop that is going to do the following line if the condition (10 line) meet.
+    print(string) # Pring the string which is defined to be the input from user
